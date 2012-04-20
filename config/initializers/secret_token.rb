716771e9d4dc8381ba0efce912c94bbaad34e2f4
koa2000 <konpero@gmail.com>
2012-04-20 23:51:36 +0900

    study

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Study::Application.config.secret_token = '99b97d1d966b410b4c476814a8bebbdb36e9cd07356be5456ac869278520712a974f2b80d2d57cf75f910514a99d9e364391d7918bc83bb45dbf0c0a1e1e28c5'
